c9d5eaea47a48d05e81a21ce27c19ade093040c3
Pecev Gligor <gilgor.pecev@breitling.com>
2025-04-23 15:27:13 +0800

    update how it works section

--- app/landingPageSections/HowItWorks/index.tsx
+++ app/landingPageSections/HowItWorks/index.tsx
@@ -13,17 +13,17 @@ const HowItWorks = () => {
                 <Section
                     title="We Pick"
                     text="Each month's we handpick exciting snacks from across China, add insert in English about their nutritional ingrediants and add surprise cultural item to make it truly special."
-                    imageSrc="/curate_section/we_curate.webp"
+                    imageSrc="/panda_character/we_pick.webp"
                 />
                 <Section
                     title="You Subscribe"
                     text="Choose your box style and plan. Whether you want classic comfort or bold adventures, we’ve got a journey ready for your tastebuds."
-                    imageSrc="/curate_section/subscribe.webp"
+                    imageSrc="/panda_character/you_subscribe.webp"
                 />
                 <Section
                     title="We Deliver"
                     text="Sit back and relax while we ship your box straight to your door — filled with flavors, stories, and a bit of magic from the other side of the world."
-                    imageSrc="/curate_section/we_deliver.webp"
+                    imageSrc="/panda_character/we_deliver.webp"
                 />
             </article>
             <div className="mt-20 flex w-full items-center justify-center"></div>
--- app/landingPageSections/IntoPhotography.tsx
+++ app/landingPageSections/IntoPhotography.tsx
@@ -8,7 +8,7 @@ const IntroPhotography = () => {
             className="relative mt-[82px] flex h-[calc(100vh-var(--navbar-height))] w-full flex-col bg-primary-red md:flex-row"
         >
             <HeroTextOverlay />
-            <div className="relative h-full w-full md:w-1/2">
+            <div className="relative mb-4 h-full w-full md:w-1/2">
                 <Image
                     src="/product_images/bg_removed/whatis_inside_cropped_1.webp"
                     alt="chinese snacks"

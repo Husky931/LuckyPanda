97d6ca45502ae8c0455b38f6512e4c4aad829c37
Pecev Gligor <gilgor.pecev@breitling.com>
2025-11-02 14:57:50 +0800

    candy_shot_2 image increase size for mobile and remove image watermark

--- app/components/CandyShot.tsx
+++ app/components/CandyShot.tsx
@@ -6,11 +6,11 @@ const CandyShot = () => {
     return (
         <div className="relative w-full">
             <Image
-                src="/candy_shot.webp"
+                src="/candy_shot_2.webp"
                 alt="Candy Shot"
                 width={1920}
                 height={800}
-                className="h-auto w-full object-cover"
+                className="h-[60vh] w-full object-cover sm:h-auto"
                 priority
             />
         </div>

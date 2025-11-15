f71c0d15529c249197fa8a5fed510f39a7cfb56f
Pecev Gligor <gilgor.pecev@breitling.com>
2025-11-16 07:00:02 +0800

    remove single box

--- app/landingPageSections/ChoosePlan/index.tsx
+++ app/landingPageSections/ChoosePlan/index.tsx
@@ -72,16 +72,18 @@ const ChooseYourPlan = () => {
             </div>
 
             {/* OR separator */}
-            <div className="my-12 flex w-full items-center justify-center">
+
+            {/* <div className="my-12 flex w-full items-center justify-center">
                 <div className="h-px w-full border-t-2 border-dotted border-gray-400"></div>
                 <span className="mx-2 text-5xl uppercase text-gray-400">
                     OR
                 </span>
                 <div className="h-px w-full border-t-2 border-dotted border-gray-400"></div>
-            </div>
+            </div> */}
 
             {/* One-time Purchase Option */}
-            <div className="flex justify-center">
+
+            {/* <div className="flex justify-center">
                 <SelectPlan
                     title="Single Box"
                     price="$39.99"
@@ -94,7 +96,7 @@ const ChooseYourPlan = () => {
                         )
                     }
                 />
-            </div>
+            </div> */}
         </section>
     )
 }

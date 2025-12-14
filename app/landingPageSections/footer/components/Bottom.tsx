d1c5e8137ccd7cca7fec6ce261c3ca1c6b73e6bb
Pecev Gligor <gilgor.pecev@breitling.com>
2025-12-14 12:20:55 +0800

    add pixa in footer

--- app/landingPageSections/footer/components/Bottom.tsx
+++ app/landingPageSections/footer/components/Bottom.tsx
@@ -1,9 +1,19 @@
 const Bottom = () => {
     return (
-        <div className="sm:flex sm:items-center sm:justify-between">
+        <div className="flex-col sm:flex sm:items-start sm:justify-between">
             <span className="text-sm text-gray-400 sm:text-center">
                 Copyright © Lucky Panda Treats™ Operating Company, LLC. All
                 Rights Reserved
+            </span>
+            <span className="text-sm text-gray-400 sm:text-center">
+                Product of&nbsp;
+                <a
+                    className="underline"
+                    target="_blank"
+                    href="https://pixaventures.com/"
+                >
+                    pixaventures.com
+                </a>
             </span>
         </div>
     )
